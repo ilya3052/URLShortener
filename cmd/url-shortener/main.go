419dd207dd53a8d0397ae91f885be6c8997d2d94
ilya3052 <ipopadin5@gmail.com>
2024-09-12 19:06:05 +0300

    adding router

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,15 +7,18 @@ import (
 	"url-shortener/internal/config"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/postgresql"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
-func main () {
+func main() {
 	fmt.Println("Запущено")
 
 	cfg := config.MustLoad()
@@ -33,42 +36,35 @@ func main () {
 		os.Exit(1)
 	}
 
-	id, err := storage.SaveURL("https://google.com", "google")
-	if err != nil {
-		log.Error("failed to save url", sl.Err(err))
-		os.Exit(1)
-	}
-	log.Info("saved url", slog.Int64("id", id))
+	// id, err := storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+	// log.Info("saved url", slog.Int64("id", id))
+	// url, err := storage.GETUrl("https://google.com")
+	// if err != nil {
+	// 	log.Error("failed to get url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+	// log.Info("get url", slog.String("url", url))
 
-	url, err := storage.GETUrl("https://google.com")
+	router := chi.NewRouter()
 
-	if err != nil {
-		log.Error("failed to get url", sl.Err(err))
-		os.Exit(1)
-	}
-
-	log.Info("get url", slog.String("url", url))
-
-	err = storage.DeleteURL("https://google.com")
-
-	if err != nil {
-		log.Error("failed to delete url", sl.Err(err))
-		os.Exit(1)
-	}
-
-	log.Info("deleted url")
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
 
 	_ = storage
 }
 
-func setupLogger (env string) *slog.Logger {
+func setupLogger(env string) *slog.Logger {
 
 	var log *slog.Logger
 
 	switch env {
 	case envLocal:
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-		
+
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.0
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
